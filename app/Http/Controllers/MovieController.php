17742cf5583a86bc221523910a1d231273ea41e1
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-22 16:24:01 +0200

    passo la tabella movies dal db

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    // Restituisco la lista dei film
+    public function index()
+    {
+        $movies = Movie::all();
+        dd($movies);
+        return view('movies.index', compact('books'));
+    }
 }
