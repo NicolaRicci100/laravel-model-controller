e7c21789e7bb54cc7d637fd4d31613cb4c594c0f
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-08-22 17:00:41 +0200

    stampo elenco dei film grezzo

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,7 +11,7 @@ class MovieController extends Controller
     public function index()
     {
         $movies = Movie::all();
-        dd($movies);
-        return view('movies.index', compact('books'));
+        // dd($movies);
+        return view('movie', compact('movies'));
     }
 }
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Document</title>
+    @vite('resources/js/app.js')
 </head>
 
 <body>
-    <h1>TEST</h1>
+    @yield('main')
 </body>
 
 </html>
